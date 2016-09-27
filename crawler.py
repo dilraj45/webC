29c064fe29738f548a705b7fc1e63dc2af4b9212
dilraj45 <dilraj.singh850@gmail.com>
2016-09-27 18:48:15 +0530

    integerated summarizer with crawler.py

--- crawler.py
+++ crawler.py
@@ -4,6 +4,8 @@ import re
 from sets import Set
 import bs4
 from getSource import getSource
+from summarizer import summarizer
+from pymongo import MongoClient
 import requests
 
 visited = Set()
@@ -18,6 +20,7 @@ er_file = open('errors.txt', 'w')
 
 # Creating an object of class getSource
 req_obj = getSource()
+sum_obj = summarizer()
 
 
 def add_links_to_queue(url):
@@ -25,7 +28,7 @@ def add_links_to_queue(url):
         sp = bs4.BeautifulSoup(req_obj.get_html_text(url), 'lxml')
         for tag in sp.find_all('a', href=True):
             base = 'http://' + req_obj.get_base_hostname()
-            print "Base url: "+base
+            print "Base url: " + base
             link = urljoin(base, tag['href'])
             if re.match(pattern, link) is not None:
                 if link not in visited:
@@ -47,6 +50,7 @@ def bfs(level):
     i = 0
     while i < length:
         add_links_to_queue(queue[0])
+        sum_obj.create_and_index_summary(queue[0])
         queue.pop(0)
         i = i + 1
     bfs(level - 1)
@@ -58,4 +62,19 @@ def bfs_level(url_begin, level):
     bfs(level)
 
 
-bfs_level('http://web.mit.edu', 4)
+def database_setup():
+    # setting up the database
+    client = MongoClient('localhost', 27017)
+    db = client['test_project']
+    col = db['summary']
+    keys = open('stems.txt', 'r').read().split('\n')
+    col.insert({"_id": "hashmap",
+                "total_url": 0,
+                "hash": {}})
+    for word in keys:
+        post = '{"_id":"' + word + '","postings":[]}'
+        col.insert(post)
+
+if __name__ == '__main__':
+    database_setup()
+    bfs_level('http://web.mit.edu', 5)
