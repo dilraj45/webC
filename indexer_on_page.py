27ecaa800fd36871dfb688ccccc5949c6addf076
dilraj45 <dilraj.singh850@gmail.com>
2016-10-03 05:27:42 +0530

    Indexer for on page features

--- indexer_on_page.py
+++ indexer_on_page.py
@@ -1,50 +1,62 @@
+""" THIS module is for indexing the on page features of a URL
+ here a collection is formed and each word is treated as document.
+ Each document has a id from which it can be accessed. With each word
+ a posting list is associated which has two attributes first the id of
+ the URL and 2nd count of the number of times a word
+ appears in a given URL """
+
+
 from pymongo import MongoClient
 from stemming.porter2 import stem
 from get_text_from_tag_for_url import get_individual_tags_text
+import re
 
 
 class on_page_summarizer:
 
-    def __init__(self):
+    def __init__(self, src_content, url):
         # for connection establishing
         self.client = MongoClient('localhost', 27017)
-        # project name
-        self.db = self.client['test_p']
-        # creating collections for every word in the keyword list
-        # reading form file
-        self.fp = open('stems.txt', 'r').read().split('\n')
-        # for getting text of a given url
-        self.get_obj = get_individual_tags_text()
 
-        # for keyword in self.fp:
+        # project name
+        self.db1 = self.client['test_project']
+
+        # reading from file
+        self.fp = open('stems.txt', 'r').read().split('\n')
+
+        # for getting text of a given url
+        self.get_obj = get_individual_tags_text(src_content)
+        for keyword in self.fp:
             # here collections are made automatically here
-            # print keyword
-            #self.creating_documents_for_every_word(keyword)
+            self.creating_documents_for_every_word(keyword)
             # 1st attribute is for word's id and 2nd for posting list
 
-    def creating_documents_for_every_word(self, keyword):
-        # using keywor das our id
-        # for every keywor dwe need to store a opsting list
-        try:
-            self.db[keyword].insert(
-             {"_id": keyword + "title", "postings": []})  # [] null list
-            self.db[keyword].insert({"_id": keyword + "meta", "posting": []})
-        except Exception as e:
-            print e    
-        # for finding a document with a given id
-        # doc = self.db[keyword].find_one({"_id": "title"})
+        # to find the document for finding url an did mapping
+        self.doc = self.db1.summary.find_one({"_id": "_hashmap"})
 
-        # self.db.keyword.update(
-        #     {"_id": "title"},
-        #     {"postings": new_list})
+        # to get the dictionary
+        self.dic = self.doc['mapping']
+        # getting the id of URL
+        url = re.sub(r',', r';', url)
+        self.id_of_url = self.dic[url]
+
+    def creating_documents_for_every_word(self, keyword):
+        # using keyword as our id
+        # for every keyword we need to store a posting list
+        try:
+            self.db1.on_page_summary.insert(
+                {"_id": keyword + "_title", "posting": []})
+            self.db1.on_page_summary.insert(
+                {"_id": keyword + "_meta", "posting": []})
+            self.db1.on_page_summary.insert(
+                {"_id": keyword + "_header", "posting": []})
+        except Exception as e:
+            print "Exception occured " + e
+            return
 
     def get_dict_words(self, on_page_summary_for_given_tag):
-        print on_page_summary_for_given_tag
         word_stems = []
-        try:
-            stemmed_words_list = on_page_summary_for_given_tag.lower().split()
-        except Exception as e:
-            print e
+        stemmed_words_list = on_page_summary_for_given_tag.lower().split()
         for word in stemmed_words_list:
             word_stems.append(stem(word))
         key_dic = {}
@@ -58,37 +70,47 @@ class on_page_summarizer:
         return key_dic
 
     # HERE id of url is generated before it is called
-    def add_to_db_posting(self, keyword, count, tag, id_URL):
-        self.db[keyword].update
-        (
-            {"_id": keyword + tag},
-            {"posting", [id_URL, count]}
+    def add_to_db_posting(self, keyword, count, tag):
+        doc = self.db1.on_page_summary.find_one({"_id": keyword + "_" + tag})
+        posting_list = doc['posting']
+        # adding the new value at coorect position in the list
+        index = 0
+        while index < len(posting_list):
+            index = index + 1
+        posting_list.append([self.id_of_url, count])
+        # updating the list in database
+        self.db1.on_page_summary.update(
+            {"_id": keyword + "_" + tag},
+            {"posting": posting_list}
         )
 
     def for_title(self):
 
         title_text = self.get_obj.get_title_text()
-        print "fudu"+title_text
         # convering the dictionary to key, value pairs stored in a list
         key_dic = {}
         key_dic = self.get_dict_words(title_text)
+        print key_dic['faculti']
         for word in key_dic:
-            self.add_to_db_posting(word, key_dic[word], "title", id_URL)
+            self.add_to_db_posting(word, key_dic[word], "title")
 
     def for_meta(self):
         meta_text = self.get_obj.get_meta_text()
-
         key_dic = {}
-        key_dic = obj.get_dict_words(meta_text)
+        key_dic = self.get_dict_words(meta_text)
         # convering the dictionary to key, value pairs stored in a list
         for word in key_dic:
-            self.add_to_db_posting(word, key_dic[word], "meta", id_URL)
+            self.add_to_db_posting(word, key_dic[word], "meta")
+
+    def for_header(self):
+        header_text = self.get_obj.get_meta_text()
+        key_dic = {}
+        key_dic = self.get_dict_words(header_text)
+        for word in key_dic:
+            self.add_to_db_posting(word, key_dic[word], "header")
 
     def index_on_page_summary(self):
         # for every tag
         self.for_title()
         self.for_meta()
-
-
-obj = on_page_summarizer()
-obj.index_on_page_summary()
+        self.for_header()
