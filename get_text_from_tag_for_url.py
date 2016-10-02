26314aa79bd719bb31f472fc3675a249f3b2ee4a
dilraj45 <dilraj.singh850@gmail.com>
2016-10-03 05:27:22 +0530

    Descption generation for on oage features

--- get_text_from_tag_for_url.py
+++ get_text_from_tag_for_url.py
@@ -1,35 +1,72 @@
-from getSource import*
-from stemming.porter2 import stem
+# this module is for finding the ktml text for a given url
+# here in this text of individua ltags can found
+
 import bs4
+import re
 
 
 class get_individual_tags_text:
 
-    get_individual_tags_text(self):
-        getSource get_source_obj = new getSource()
-        self.soup = bs4.BeautifulSoup(getSource_obj.get_html_text(url), 'lxml')
-        html_text = get_source_obj.get_html_text(URL)
+    def __init__(self, src_content):
+        self.soup = bs4.BeautifulSoup(src_content, "lxml")
 
     def get_title_text(self):
+        text = ""
         try:
-            for title_tag in self.soup_handler.find_all('title', href=True):
+            for title_tag in self.soup.find_all('title'):
                 text = text + title_tag.get_text()
-        except Exception as e:
-            # for debugging
-            print e
-            return text
+
+        except TypeError:
+            pass
+        text = re.sub(r'@', r' @ ', text.encode('utf -8'))
+        text = re.sub(r'[^a-zA-Z0-9@ ]', '', text.encode('utf -8'))
+        return text
 
     def get_meta_text(self):
+        text = ""
         try:
-            for meta_tag in self.soup_handler.find_all('meta', href=True):
-                text = text + meta_tag.get_text()
-        except Exception as e:
-            # for debugging
-            print e
-            return text
+            for meta_tag in self.soup.find_all('meta'):
+                text = text + meta_tag['content']
+        except TypeError:
+            pass
+        text = re.sub(r'@', r' @ ', text.encode('utf -8'))
+        text = re.sub(r'[^a-zA-Z0-9@ ]', '', text.encode('utf -8'))
+        return text
 
-# html_text = obj.get_html_text(url)
-# obj.get_html_text(URL)
+    def get_table_text(self):
+        text = ""
+        try:
+            for table_tag in self.soup.find_all('table'):
+                text = text + table_tag.get_text()
+        except TypeError:
+            pass
+        text = re.sub(r'@', r' @ ', text.encode('utf -8'))
+        text = re.sub(r'[^a-zA-Z0-9@ ]', '', text.encode('utf -8'))
+        return text
 
+    def get_header_text(self):
+        text = ""
+        try:
+            for header_tag in self.soup.find_all('h1'):
+                text = text + header_tag.get_text()
 
-# get_individual_tags_text  tags_Object = new get_individual_tags_text()
+            for header_tag in self.soup.find_all('h2'):
+                text = text + header_tag.get_text()
+
+            for header_tag in self.soup.find_all('h3'):
+                text = text + header_tag.get_text()
+
+            for header_tag in self.soup.find_all('h4'):
+                text = text + header_tag.get_text()
+
+            for header_tag in self.soup.find_all('h5'):
+                text = text + header_tag.get_text()
+
+            for header_tag in self.soup.find_all('h6'):
+                text = text + header_tag.get_text()
+        except TypeError:
+            pass
+        text = re.sub(r'@', r' @ ', text.encode('utf -8'))
+        text = re.sub(r'[^a-zA-Z0-9@ ]', '', text.encode('utf -8'))
+        # print text
+        return text
